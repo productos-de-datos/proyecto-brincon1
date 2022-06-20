aaaabadf719d93b3226f845bd44104e9412438c6
Brenda Rincon Rico <101733090+brincon1@users.noreply.github.com>
2022-06-20 12:42:42 -0500

    prueba pregunta2

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -14,24 +14,33 @@ def ingest_data():
 
     """
 
-    import wget
+    #import wget
     import os
 
     # se especifica el lugar donde se guardarán los archivos descargados
-    os.chdir('data_lake/landing/')
+    #rutaDescarga = os.chdir('data_lake/landing/')
 
     # Dado que los archivos que se quieren descargar estan en la misma ruta y son numeros consecutivos se hace un ciclo for
     # para acceder a obtener la url de cada archivo. Además, como tenemos dos archivos xls (2016 y 2017) se hace un condicional 
     # para obtener la url de dichos archivos. Como ya tenemos las url, con wget descargamos todos los archivos.
 
+    # for num in range(1995, 2022):
+    #     if num in range(2016, 2018):
+    #         urlArchivosXls = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xls?raw=true'.format(num)
+    #         wget.download(urlArchivosXls)
+    #     else:
+    #         urlArchivosXlsx = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xlsx?raw=true'.format(num)
+    #         wget.download(urlArchivosXlsx )
+
     for num in range(1995, 2022):
         if num in range(2016, 2018):
-            urlArchivosXls = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xls?raw=true'.format(num)
-            wget.download(urlArchivosXls)
+            #urlArchivosXls = https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xls?raw=true.format(num)
+            curl https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xls?raw=true.format(num) -o data_lake/landing/{}.xls.format(num)
         else:
-            urlArchivosXlsx = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xlsx?raw=true'.format(num)
-            wget.download(urlArchivosXlsx )
-
+            #urlArchivosXlsx = https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xlsx?raw=true.format(num)
+            curl https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xlsx?raw=true.format(num) -o data_lake/landing/{}.xls.format(num)
+    
+    #curl https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/1995.xlsx?raw=true -o data_lake/landing/1995.xlsx
     #raise NotImplementedError("Implementar esta función")
 
 
