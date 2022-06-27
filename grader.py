85620cf5e1de55ce348eebdc74eff40eeb463b77
Brenda Rincon Rico <101733090+brincon1@users.noreply.github.com>
2022-06-27 08:25:38 -0500

    rm rf

--- grader.py
+++ grader.py
@@ -108,6 +108,7 @@ def test_06():
 def test_07():
     """Evalua el pipeline"""
     os.system("rm -rf data_lake")
+    os.system("make create_data_lake")
     os.system("make pipeline")
     assert os.path.isfile("data_lake/business/precios-diarios.csv") is True
     assert os.path.isfile("data_lake/business/precios-mensuales.csv") is True
